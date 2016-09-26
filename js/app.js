5f9a116b13b9268b092493fb044caed8a70488ef
diananr <navarroruiz.diana>
2016-09-26 07:43:46 -0500

    agregando cambios en el formulario

--- js/app.js
+++ js/app.js
@@ -1,17 +1,65 @@
 window.addEventListener("load", function (){
-	var posEyes = document.getElementById("ojosL");
-	var eyes = document.getElementById("negros");
+	var cejas = document.getElementById("cajaCejas");
+	var ojos = document.getElementById("cajaOjos");
+	var labios = document.getElementById("cajaLabios");
+	var nariz = document.getElementById("cajaNariz");
+	var posCej = document.getElementById("posCejas");
+	var posOj = document.getElementById("posOjos");
+	var posLab = document.getElementById("posLabios");
+	var posNar = document.getElementById("posNariz");
 
-	eyes.addEventListener("dragstart", function(e){
-		e.dataTransfer.setData("text",e.target.id);
+	cejas.addEventListener("dragstart", function(e){
+		e.dataTransfer.setData("text", e.target.id);
 	});
-	posEyes.addEventListener("dragover", function(e){
+	posCej.addEventListener("dragover", function(e){
 		e.preventDefault();
 	});
-	posEyes.addEventListener("drop", function(e){
+	posCej.addEventListener("drop", function(e){
+		e.preventDefault();
+		var data = e.dataTransfer.getData("text");
+		e.target.appendChild(document.getElementById(data));
+	});
+
+	ojos.addEventListener("dragstart", function(e){
+		e.dataTransfer.setData("text",e.target.id);
+	});
+	posOj.addEventListener("dragover", function(e){
+		e.preventDefault();
+	});
+	posOj.addEventListener("drop", function(e){
 		e.preventDefault();
 		var data = e.dataTransfer.getData("text");
     	e.target.appendChild(document.getElementById(data));
+	});
+
+	labios.addEventListener("dragstart", function(e){
+		e.dataTransfer.setData("text",e.target.id);
+	});
+	posLab.addEventListener("dragover", function(e){
+		e.preventDefault();
+	});
+	posLab.addEventListener("drop", function(e){
+		e.preventDefault();
+		var data = e.dataTransfer.getData("text");
+    	e.target.appendChild(document.getElementById(data));
+	});
+
+	nariz.addEventListener("dragstart", function(e){
+		e.dataTransfer.setData("text",e.target.id);
+	});
+	posNar.addEventListener("dragover", function(e){
+		e.preventDefault();
+	});
+	posNar.addEventListener("drop", function(e){
+		e.preventDefault();
+		var data = e.dataTransfer.getData("text");
+    	e.target.appendChild(document.getElementById(data));
+	});
+
+	var btn = document.getElementById("boton");
+	btn.addEventListener("click", function(e){
+		var msj = document.getElementById("mensaje");
+		msj.style.display = "block"
 	});
 });
 
@@ -21,4 +69,4 @@ window.onload = function() {
 		var y = -(e.clientY/125);
 	this.body.style.backgroundPosition = x + 'px ' + y + 'px';
   };
-};
+}
